60c3277fa159f72ff3251a094a314d5f7e76fddd
CesarPalma11 <cesarpalmag16@gmail.com>
2023-11-13 00:45:43 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from "./Navbar";
+import Rout from './rout';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar />
+      <Rout />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <header>
+        <img src='./img/jabon1.png' alt=''></img>
+        <img src='./img/jabon2.png' alt=''></img>
+
+    </header>
+  )
+}
+
+export default Home
